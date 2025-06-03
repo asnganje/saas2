d8253de9785bad0040428fc96e2ab2d03e3447fa
asnganje <abd.nganje@gmail.com>
2025-06-03 20:43:45 +0300

    add logic to toggle sidebar

--- app/javascript/controllers/hide_controller.js
+++ app/javascript/controllers/hide_controller.js
@@ -2,8 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="hide"
 export default class extends Controller {
-  static targets = ["profileCard"]
+  static targets = ["profileCard", "sideBar"]
   toggleProfileCard() {
-    this.profileCardTarget.classList.toggle("hidden")
+    if (this.hasProfileCardTarget) {
+      this.profileCardTarget.classList.toggle("hidden")
+    }
+  }
+
+  toggleSideBar() {
+    if(this.hasSideBarTarget) {
+      this.sideBarTarget.classList.toggle("hidden")
+    }
   }
 }
