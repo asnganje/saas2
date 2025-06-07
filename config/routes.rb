d2b597754006f57f383ecc0684dcd586b6defc2a
asnganje <abd.nganje@gmail.com>
2025-06-07 09:29:33 +0300

    modify project routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :projects, only: [ :index, :create ]
+  resources :projects, only: [ :index, :create, :destroy ]
   devise_for :users
   resources :dashboard, only: [ :index ]
   get "up" => "rails/health#show", as: :rails_health_check
