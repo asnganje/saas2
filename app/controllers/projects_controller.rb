15b35b63d17cb5287096b16b4cb8f404845ad403
asnganje <abd.nganje@gmail.com>
2025-06-07 09:28:25 +0300

    add key actions to projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,42 @@
 class ProjectsController < ApplicationController
   layout 'admin'
+  before_action :set_project, only: [:destroy]
   def index
-    @projects = []
-    10.times.each do |i|
-      @projects << "project#{i}"
+    @projects = Project.all
+  end
+
+  def create
+    @project = current_user.projects.create(project_params)
+    if @project.save
+      respond_to do |format|
+        format.html {redirect_to projects_url, notice: "Project created!"} 
+      end      
+    else
+      respond_to do |format|
+        format.html {redirect_to projects_url, alert: @project.errors.full_messages[0]}
+      end 
     end
+    
+  end
+
+  def destroy
+    if @project.destroy
+      respond_to do |format|
+        format.html {redirect_to projects_url, notice: "Project deleted"}
+      end
+    else
+      respond_to do |format|
+        format.html {redirect_to projects_url, alert: @project.errors.full_messages[0]}
+      end
+    end
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
   end
 end
